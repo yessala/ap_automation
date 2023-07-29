e8a6da06fef59b32e2de83452b348b0f40d07e17
Yessalim Salazar <121966068+yessala@users.noreply.github.com>
2023-07-29 11:29:03 -0300

    configurando selenium

---
+++ config/src/main/java/yess/config/Application.java
@@ -0,0 +1,8 @@
+package yess.config;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ config/src/test/java/yess/config/ApplicationTest.java
@@ -0,0 +1,13 @@
+package yess.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ApplicationTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
